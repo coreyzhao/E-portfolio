3e4fd0475d818257be6ef2d07722b6b902650a12
Corey Zhao <coreyzh2004@gmail.com>
2024-07-31 10:47:53 -0400

    UI: navbar spacing changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,12 +33,12 @@ const Navbar = () => {
 
   return (
     <div
-      className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+      className={`fixed top-0 left-0 w-full z-50 border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
         openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
       }`}
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
-        <div className="flex flex-row w-[12rem] xl:mr-8" >
+        <div className="flex flex-row w-[12rem] xl:mr-8">
           {/* <a href="#hero">
             <p>Corey Zhao</p>
           </a> */}
@@ -46,15 +46,14 @@ const Navbar = () => {
             <img src={linkedin} width={22} height={22}/>
           </a>
           <a className="pl-5" href="https://github.com/coreyzhao" target="blank">
-          <img src={github} width={22} height={22}/>
+            <img src={github} width={22} height={22}/>
           </a>
         </div>
-        
-
+  
         <nav
           className={`${
             openNavigation ? "flex" : "hidden"
-          } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent `}
+          } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:ml-auto lg:bg-transparent`}
         >
           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
             {navigation.map((item) => (
@@ -65,24 +64,15 @@ const Navbar = () => {
                 className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
                   item.onlyMobile ? "lg:hidden" : ""
                 } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
-                  item.url === pathname.hash
-                    ? "z-2 lg:text-n-1"
-                    : "lg:text-n-1/50"
-                } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
+                  item.url === pathname.hash ? "z-2 lg:text-n-1" : "lg:text-n-1/50"
+                } lg:leading-5 lg:hover:text-n-1 xl:px-12 lg:px-10`}
               >
                 {item.title}
               </a>
             ))}
           </div>
-
-          <HamburgerMenu />
         </nav>
-
-        
-        {/* <Button className="hidden lg:flex" href="#login">
-          ENG / FR
-        </Button> */}
-
+  
         <Button
           className="ml-auto lg:hidden"
           px="px-3"
@@ -93,6 +83,6 @@ const Navbar = () => {
       </div>
     </div>
   );
-};
+}  
 
 export default Navbar;
