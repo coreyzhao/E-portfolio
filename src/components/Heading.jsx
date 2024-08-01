74587e3baa7f5503e1ff9682d7a89f8055cc9b88
Corey Zhao <coreyzh2004@gmail.com>
2024-08-01 10:52:03 -0400

    UI: added inter font

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -6,7 +6,7 @@ const Heading = ({ className, title, text, tag }) => {
       className={`${className} max-w-[50rem] mx-auto px-5 mb-12 lg:mb-10 md:text-center`}
     >
       {tag && <TagLine className="mb-4 md:justify-center">{tag}</TagLine>}
-      {title && <h3 className="h3">{title}</h3>}
+      {title && <h3 className="h3  font-bold">{title}</h3>}
       {text && <p className="body-2 mt-4 text-n-4">{text}</p>}
     </div>
   );
