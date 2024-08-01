3531795ddcae1d0c774fd51c6a36434ba5168fba
Corey Zhao <coreyzh2004@gmail.com>
2024-08-01 11:00:50 -0400

    UI: Titles changed to inter font

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -21,14 +21,14 @@ const Experience = () => {
           
 
           <div className="relative z-1 grid gap-5 lg:grid-cols-2">
-            <div className="relative min-h-[30rem] border border-n-1/10 rounded-3xl overflow-hidden">
+            <div className="relative min-h-[28rem] border border-n-1/10 rounded-3xl overflow-hidden">
               <div className="absolute inset-0">
                 
               </div>
 
               <div className="absolute inset-0 flex flex-col justify-start p-8 bg-gradient-to-b from-n-8/0 to-n-8/90 lg:p-15">
-                <h4 className="h4 mb-6">Software Developer Intern at Lottus</h4>
-                <p className="body-2 mb-[20px] text-n-3 pr-10">
+                <h4 className="h4 mb-6 ">Software Developer Intern at Lottus</h4>
+                <p className="body-2 mb-[20px] text-n-3 pr-10 font-inter font-medium">
                   Build and maintain critical React components used in Lottus' frontend application. Work Closely with backend developement team in order to implement and handle dynamic api requests
                 </p> 
                 <div className="flex flex-wrap justify-start gap-2 mb-5 pr-10">
@@ -38,7 +38,7 @@ const Experience = () => {
                     
                     key={item.id}
                   >
-                    <h7 className="text-xs align-middle">{item.title}</h7>
+                    <h7 className="text-xs align-middle font-inter font-medium">{item.title}</h7>
                   </div>
                 ))}
               </div>
@@ -51,14 +51,14 @@ const Experience = () => {
               
             </div>
 
-            <div className="relative min-h-[30rem] border border-n-1/10 rounded-3xl overflow-hidden">
+            <div className="relative min-h-[28rem] border border-n-1/10 rounded-3xl overflow-hidden">
               <div className="absolute inset-0">
                 
               </div>
 
               <div className="absolute inset-0 flex flex-col justify-start p-8 bg-gradient-to-b from-n-8/0 to-n-8/90 lg:p-15">
                 <h4 className="h4 mb-6">Software Developer for BlockChain at McGill</h4>
-                <p className="body-2 mb-[20px] text-n-3">
+                <p className="body-2 mb-[20px] text-n-3 font-inter font-medium">
                 Attending a Solidity Bootcamp where I am learning about smart contracts, decentralized applications
 	and blockchain architecture. Attended an online bootcamp where I learned the basics of Typescript.
                 </p>
@@ -69,7 +69,7 @@ const Experience = () => {
                     
                     key={item.id}
                   >
-                    <h7 className="text-xs align-middle">{item.title}</h7>
+                    <h7 className="text-xs align-middle font-inter font-medium">{item.title}</h7>
                   </div>
                 ))}
                 </div>
